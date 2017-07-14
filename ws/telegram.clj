26bd623bde79bab5efb8ee28a354ecca9e92d38b
Quan Nguyen <huu2uan@gmail.com>
2017-07-14 19:08:53 +0700

    cleanup

--- ws/telegram.clj
+++ ws/telegram.clj
@@ -3,11 +3,8 @@
 ;; **
 ;;; # Gorilla REPL
 ;;; 
-;;; Welcome to gorilla :-)
-;;; 
-;;; Shift + enter evaluates code. Hit alt+g twice in quick succession or click the menu icon (upper-right corner) for more commands ...
-;;; 
-;;; It's a good habit to run each worksheet in its own namespace: feel free to use the declaration we've provided below if you'd like.
+;;; + Data is dump from Telegram, using [telegram-history-dump](https://github.com/tvdstaaij/telegram-history-dump) utility (for confidential reasons, this data is not available on repo).
+;;; + Data is process by Clojure & visualize using [ggjoy](https://github.com/clauswilke/ggjoy) R library.
 ;; **
 
 ;; @@
