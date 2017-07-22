6189c9c9f5cb2eae1ca4093fd814bdfb6a84e2e9
Quan Nguyen <huu2uan@gmail.com>
2017-07-23 02:29:39 +0700

    imo

--- src/gorilla_samples/util.clj
+++ src/gorilla_samples/util.clj
@@ -1,10 +1,19 @@
 (ns gorilla-samples.util
   (:require
     [clj-time.core :as t]
-    [clj-time.coerce :as c]))
-
+    [clj-time.coerce :as c]
+    [clojure.data.csv :as csv]
+    [clojure.java.io :as io]))
+    
 (defn ->vn-time [t]
   (t/to-time-zone (c/from-long (* t 1000)) (t/time-zone-for-offset 7)))
 
 (defn update-values [m f & args]
   (into {} (for [[k v] m] [k (apply f v args)])))
+
+(defn read-csv [f]
+  (with-open [reader (io/reader (io/resource f))]
+    (doall
+      (let [[header & data] (csv/read-csv reader)
+            header-kw       (map keyword header)]
+        (map #(zipmap header-kw %) data)))))
