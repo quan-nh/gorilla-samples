cab3f002615d3d8cb78435258d10f17bbbd0413d
Quan Nguyen <huu2uan@gmail.com>
2017-07-23 14:12:58 +0700

    imo

--- src/gorilla_samples/util.clj
+++ src/gorilla_samples/util.clj
@@ -8,10 +8,18 @@
 (defn ->vn-time [t]
   (t/to-time-zone (c/from-long (* t 1000)) (t/time-zone-for-offset 7)))
 
+(defn ->int [s]
+  (try (Integer/parseInt s) (catch Exception _ 0)))
+  
 (defn update-values [m f & args]
   (into {} (for [[k v] m] [k (apply f v args)])))
 
 (defn read-csv [f]
+  (with-open [reader (io/reader (io/resource f))]
+    (doall
+      (csv/read-csv reader))))
+
+(defn read-csv-with-header [f]
   (with-open [reader (io/reader (io/resource f))]
     (doall
       (let [[header & data] (csv/read-csv reader)
